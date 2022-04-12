747e10ce6589dc2b914ded2b2b2c70df7e88e626
SavelovSerhii <savelov12111@gmail.com>
2022-04-12 18:07:12 +0300

    First version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const message = document.getElementsByClassName('hidden-message')[0];
+const errorMessage = document.getElementsByClassName('error-message')[0];
+const logo = document.getElementsByClassName('logo')[0];
+const promise1 = new Promise(iDontKnow);
+const promise2 = new Promise(iDontKnow);
+
+function iDontKnow(resolve, reject) {
+  logo.addEventListener('click', () => {
+    message.classList.add('message');
+    resolve('Promise was resolved!');
+  });
+
+  setTimeout(() => {
+    reject('Promise was rejected!');
+  }, 3000);
+}
+
+promise1
+  .then((result) => {
+    message.innerHTML = result;
+  })
+  .catch((result) => {
+    errorMessage.innerHTML = result;
+  });
+
+promise2
+  .then((result) => {
+    message.innerHTML = result;
+  })
+  .catch((result) => {
+    errorMessage.innerHTML = result;
+  });
